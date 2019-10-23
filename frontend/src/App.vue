171f6baaa0a5381a0f2686b110bd17a1f5fadf2c
HaeInJeong <wjdgodls0827@naver.com>
2019-10-23 11:53:47 +0900

    [ADD] Intro 추가중

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,41 @@
 <template>
   <v-app>
-    <Header/>
-    <v-content>
-      <router-view/>
-    </v-content>
-    <Footer/>
+    <div v-if="first">
+      <Intro @intro="goHome"/>
+    </div>
+
+    <div v-else>
+      <Header/>
+      <v-content>
+        <router-view/>
+      </v-content>
+      <Scrolltotop/>
+      <Footer/>
+    </div>
   </v-app>
 </template>
 
 <script>
+import Intro from './views/Intro'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Scrolltotop from './components/ScrollToTop'
 
 export default {
   name: 'App',
   components:{
+    Intro,
     Header,
+    Scrolltotop,
     Footer
   },
   data () {
     return {
-      //
+      first:true
+    }
+  },
+  methods:{
+    goHome(tile){
     }
   }
 }
